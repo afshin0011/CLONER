a7d4dfab19fbe1de8b44c334501c215283bacb03
afshin0011 <uckian.kb@gmail.com>
2016-05-05 12:08:30 +0000

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -225,7 +225,7 @@ function create_config( )
     "all",
     "leave_ban",
     "admin"
-    },
+    "info",
     sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(117314950)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
